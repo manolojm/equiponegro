8c7ccceb7c523e53b7075ccc0a26f258fc36c42f
Manolo <manolojm96@gmail.com>
2018-02-09 18:19:03 +0100

    Metodos 3 y 4

--- MenuAlumnos.java
+++ MenuAlumnos.java
@@ -1,9 +1,11 @@
+
 import java.util.*;
 import java.lang.*;
 
 public class MenuAlumnos{
 	
-	static void mostrarMenu() {
+	/*** Menú ***/
+	public static void mostrarMenu() {
 		
 		System.out.println("Introduzca la opción del menú: ");
 		System.out.println("1 para dar de alta alumnos");
@@ -20,13 +22,138 @@ public class MenuAlumnos{
 		System.out.println("12 para listar faltas");
 		System.out.println("13 para salir");
 	}
+	
+	
+	/*** Metodo 3: Listar Alumnos - Manolo ***/
+	public static void listarAlumnos(ArrayList<Alumno> listaAlumnos) {
+		
+		// Ciclo para sacar los alumnos
+		for (int i = 0; i < listaAlumnos.size(); i++) {
+			
+			Alumno alumnoElegido = listaAlumnos.get(i);
+			
+			// Mostrar Alumno
+			System.out.println("Numero : " + i + " / DNI: " + alumnoElegido.getDni() + " / Nombre: " 
+			+ alumnoElegido.getNombre() + " / Apellidos: " + alumnoElegido.getApellidos() 
+			+ " \n/ Telefono: " + alumnoElegido.getTelefono() + " / Email: " + alumnoElegido.getEmail());		
+		}
+	}
+	
+	
+	/*** Metodo 4: Modificar Alumnos - Manolo ***/
+	public static void modificarAlumnos(ArrayList<Alumno> listaAlumnos) {
+		
+		Scanner entrada = new Scanner(System.in);	
+		int numerote = 1;
 
+		do {
+
+			int alumnoModifico, numerito;
+
+			System.out.println("Alumno a modificar: ");
+			alumnoModifico = entrada.nextInt();
+
+			// Buscamos al alumno elegido
+			Alumno alumnoModificar = listaAlumnos.get(alumnoModifico);
+
+			do {
+				
+				System.out.println(
+						"Que desea modificar? \n1 - DNI \n2 - Nombre" + "\n3 - Apellidos \n4 - Telefono \n5 - Email \n6 - Nada");
+				numerito = entrada.nextInt();
+
+				switch (numerito) {
+
+				case 1: // DNI
+
+					String nuevoDni;
+
+					System.out.println("Introduzca nuevo DNI: ");
+					nuevoDni = entrada.next();
+
+					alumnoModificar.setDni(nuevoDni); // Modificamos DNI
+
+					break;
+
+				case 2: // Nombre
+
+					String nuevoNombre;
+
+					System.out.println("Introduzca nuevo Nombre: ");
+					nuevoNombre = entrada.next();
+
+					alumnoModificar.setNombre(nuevoNombre); // Modificamos Nombre
+
+					break;
+
+				case 3: // Apellidos
+
+					String nuevoApellidos;
+
+					System.out.println("Introduzca nuevos Apellidos: ");
+					nuevoApellidos = entrada.next();
+
+					alumnoModificar.setApellidos(nuevoApellidos); // Modificamos Apellidos
+
+					break;
+
+				case 4: // Telefono
+
+					String nuevoTelefono;
+
+					System.out.println("Introduzca nuevo Telefono: ");
+					nuevoTelefono = entrada.next();
+
+					alumnoModificar.setTelefono(nuevoTelefono); // Modificamos Telefono
+
+					break;
+
+				case 5: // Email
+
+					String nuevoEmail;
+
+					System.out.println("Introduzca nuevo Email: ");
+					nuevoEmail = entrada.next();
+
+					alumnoModificar.setEmail(nuevoEmail); // Modificamos Email
+
+					break;
+
+				case 6: // Salir
+
+					System.out.println("Salir");
+
+					break;
+
+				default: // Por defecto
+
+					System.out.println("Opcion erronea");
+				}
+
+			} while (numerito != 6);
+			
+			do {
+				System.out.println("Desea modificar otro alumno? (1 - Si / 2 - No)");
+				numerote = entrada.nextInt();
+			} while (numerote > 2 || numerote < 1);
+			
+		} while (numerote == 1);
+	}
+
+	
+	/*** Main ***/
 	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
 		Scanner entrada = new Scanner(System.in);
 		
+		// Variables
+		ArrayList<Alumno> listaAlumnos = new ArrayList<Alumno>();
 		boolean salir = false;
 		int opcion;
+		
+		// Pruebas
+		Alumno alumno1 = new Alumno("12345X", "Antonio", "Lopez");
+		listaAlumnos.add(alumno1);
 		
 		do {
 			
@@ -48,15 +175,23 @@ public class MenuAlumnos{
 				
 				break;
 				
-			case 3:
+			case 3: // Manolo
 				
+				// Llamamos a la funcion 
+				listarAlumnos(listaAlumnos);
 				
+				// Salto de linea
+				System.out.println();
 				
 				break;
 				
-			case 4:
+			case 4: // Manolo
 				
-				
+				// Llamamos a la funcion 
+				modificarAlumnos(listaAlumnos);
+						
+				// Salto de linea
+				System.out.println();
 						
 				break;
 						
